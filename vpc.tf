267b2a95ad4b5c0aa772063640c1bd4821e3d4ec
mohammadsyed397 <mohammad.syed601@gmail.com>
2025-07-09 11:51:06 -0400

    latest

--- vpc.tf
+++ vpc.tf
@@ -130,7 +130,7 @@ resource "aws_route_table" "database" {
 resource "aws_route" "public" {
   route_table_id            = aws_route_table.public.id
   destination_cidr_block    = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.gw
+  gateway_id = aws_internet_gateway.gw.id
 }
 
 resource "aws_route" "private" {
