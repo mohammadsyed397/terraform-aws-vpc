1494a88864c32092bad422ca3cda85fdc4f5ee0f
mohammadsyed397 <mohammad.syed601@gmail.com>
2025-07-11 19:15:04 -0400

    latest

--- vpc.tf
+++ vpc.tf
@@ -60,7 +60,7 @@ resource "aws_subnet" "database" {
     var.database_subnet_tags,
     local.common_tags,
     {
-      Name = "${var.project}-${var.environment}-private-${local.az_names[count.index]}"
+      Name = "${var.project}-${var.environment}-database-${local.az_names[count.index]}"
     }
   )
   }
